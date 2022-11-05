9a4a55680b570e7ccf904ffb07a2d8289518539f
AgungBae13 <aviterdaun@gamil.com>
2022-11-05 15:05:01 +0700

    Update Bayi.php
    tes

--- application/controllers/Bayi.php
+++ application/controllers/Bayi.php
@@ -1,6 +1,6 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
-
+    //kontol
     class Bayi extends CI_Controller {
         function __construct(){
             parent::__construct();
