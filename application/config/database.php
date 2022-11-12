dcdd36af0c7a7cdf5ae71d52bea4acdc15e23d14
AgungBae13 <aviterdaun@gamil.com>
2022-11-12 21:39:21 +0700

    u

--- application/config/database.php
+++ application/config/database.php
@@ -75,13 +75,13 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
 	//Dani
 	// 'hostname' => 'localhost:8889',
 	// 'username' => 'root',
 	// 'password' => 'root',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
 	'database' => 'db_innovillage',
 	// 'hostname' => 'localhost',
 	// 'username' => 'u1606221',
